0535823ba33bc9a517d902b0d1c8ca336b29767f
guihenrry <guilhermehenry60@gmail.com>
2019-07-05 22:55:52 -0300

    modificado cores e estilo dos botões

--- js/app.js
+++ js/app.js
@@ -1,70 +1,11 @@
-// Formulario
-function initFormulario() {
-	$('.formphp').on('submit', function() {
-		var emailContato = "contato@guilhermehenrry.com.br"; // Escreva aqui o seu e-mail
-
-		var that = $(this),
-				url = that.attr('action'),
-				type = that.attr('method'),
-				data = {};
-		
-		that.find('[name]').each(function(index, value) {
-			var that = $(this),
-					name = that.attr('name'),
-					value = that.val();
-					
-			data[name] = value;
-		});
-		
-		$.ajax({
-			url: url,
-			type: type,
-			data: data,
-			success: function(response) {
-			
-				if( $('[name="leaveblank"]').val().length != 0 ) {
-					$('.formphp').html("<div id='form-erro'></div>");
-					$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
-					.hide()
-					.fadeIn(1500, function() {
-					$('#form-erro');
-					});
-				} else {
-				
-					$('.formphp').html("<div id='form-send'></div>");
-					$('#form-send').html("<span>Mensagem enviada!</span><p>Em breve eu entro em contato com você. Abraços.</p>")
-					.hide()
-					.fadeIn(1500, function() {
-					$('#form-send');
-					});
-				};
-			},
-			error: function(response) {
-				$('.formphp').html("<div id='form-erro'></div>");
-				$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
-				.hide()
-				.fadeIn(1500, function() {
-				$('#form-erro');  
-			});
-			}
-		});
-		
-		return false;
-	});
-}
-initFormulario();
 // Scrol suave 
 function initScrollSuave() {
 	const menuitems = document.querySelectorAll('.menu a[href^="#"]');
-	const btnsaibamais = document.querySelector('.intro-text a[href^="#"]');
-	
 
 	menuitems.forEach(item => {
 			item.addEventListener('click', scrollToIdOnclick);
 	})
-
-	btnsaibamais.addEventListener('click', scrollToIdOnclick);
-
+	
 	function scrollToIdOnclick(event) {
 			event.preventDefault(); 
 			const to = getScrollTopByHref(event.target) - 80; 
